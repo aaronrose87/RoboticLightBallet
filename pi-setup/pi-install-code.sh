d66d6e7c039834a8e0ef5f4390a0d7111bc605e1
Allan Doyle <allan.doyle@gmail.com>
2015-04-14 12:05:04 -0400

    Add scripts to install and run the actual control code

---
+++ pi-setup/pi-install-code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This assumes the pi-setup-primary.sh or pi-setup-secondary.sh
+# scripts have already been run.
+
+cd ~pi
+git clone https://github.com/mitmuseumstudio/RoboticLightBallet.git
+
+
---
+++ pi-setup/pi-run-code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Assumes the pi has been set up and the code has been installed.
+
+while true; do
+    cd ~pi/RoboticLightBallet/pi-code
+    python robotControl.py
+
+    # sleep is here just to make sure that if something goes
+    # horribly wrong, there might be enough CPU cycles left
+    # to ssh in and do something
+    sleep 1
+done
+
+
+
+    
