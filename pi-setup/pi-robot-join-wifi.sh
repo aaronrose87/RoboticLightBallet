efef24c1bd717a5591593b835313a8c29a0ef5fd
Allan Doyle <allan.doyle@gmail.com>
2015-04-17 12:35:03 -0400

    Add ability to loop based on argument

--- pi-setup/pi-robot-join-wifi.sh
+++ pi-setup/pi-robot-join-wifi.sh
@@ -4,20 +4,34 @@
 # If not, loop while trying to connect until it is.
 #
 
+# If called with a number, quietly loop forever, sleeping for
+# that many seconds between loops.
+
+LOOP=${1:-""}
+
 _IP=$(hostname -I) || true
 
 SSID=$(sed -n -e '/ssid/s/"//g' -e '/ssid/s/ssid=//' \
            < /etc/wpa_supplicant/wpa_supplicant.conf)
 
 
-echo -n "Connecting to $SSID wifi network"
+WIFI_JOIN() {
+    echo -n "Connecting to $SSID wifi network"
 
-while [[ ! ("$_IP" == *10.10.*) ]]; do
-    echo -n "."
-    ifdown wlan0 2>&1 | logger
-    sleep 2
-    ifup wlan0 2>&1 | logger
-    sleep 2
-    _IP=$(hostname -I) || true
-done
-echo ". IP address: $_IP"
+    while [[ ! ("$_IP" == *10.10.*) ]]; do
+        echo -n "."
+        ifdown wlan0 2>&1 | logger
+        sleep 2
+        ifup wlan0 2>&1 | logger
+        sleep 2
+        _IP=$(hostname -I) || true
+    done
+    echo ". IP address: $_IP"
+}
+
+WIFI_JOIN
+
+if [[ $LOOP ]] ; then
+    WIFI_JOIN
+    sleep $LOOP
+fi
